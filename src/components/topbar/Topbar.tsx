74154fe031b7893979f5f0ad21286fadd53799fe
Thomas E Wilson <dev.thomas.e.wilson@gmail.com>
2021-11-02 18:38:48 -0500

    Good Starting Place - Building & Stable
    Added Sass to Dev Dependencies. Removed @MUI references.

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -1,6 +1,4 @@
 import "./topbar.scss"
-import PersonIcon from '@mui/icons-material/Person';
-import MailIcon from '@mui/icons-material/Mail';
 
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
@@ -12,11 +10,9 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
                         <img src= "/assets/sxgreen.png" height="50px" alt="sxr media icon"/>
                         </a>
                      <div className="itemContainer">
-                        <PersonIcon className="icon" />
                         <span> +1 601 342 6586</span>
                     </div>
                     <div className="itemContainer">
-                        <MailIcon classname="icon" />
                         <span>sabienruffin@gmail.com</span>
                     </div>
                 </div>
