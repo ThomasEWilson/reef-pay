5260a15aa223f353d4e5b0bd1936734c4dacc65d
SMSonGitHub <sabienruffin@gmail.com>
2021-11-08 05:01:21 -0600

    Added Login Page, Other Pages

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,48 @@
-import { PageHeader } from "antd";
+import { PageHeader, Menu, Dropdown, Card, Table } from "antd";
 import React from "react";
+import { DownOutlined } from "@ant-design/icons";
+
+import Payments from "../components/payments/payments";
+import PayStats from "../components/dashboard/pay-stats";
 
 // import { Components } from "@reef-defi/react-lib";
 // import "@reef-defi/react-lib/dist/index.css";
 // const { Card, Button } = Components;
+const currencies = ["USD", "EUR", "GBP", "NZD", "BRL"];
+const menu = (
+  <Menu>
+    {currencies.map((currencyItem, indx) => (
+      <Menu.Item key={indx}>
+        <a>{currencyItem}</a>
+      </Menu.Item>
+    ))}
+  </Menu>
+);
 
 const Home = () => {
   return (
-    <PageHeader
-      title="Home"
-      className="site-page-header"
-      subTitle="Your personal Reef adobe"
-    ></PageHeader>
-    // TODO: Add Graph of Sales over Period
-    // TODO: Add Recent Transactions Table
+    <>
+      <PageHeader
+        title="Dashboard"
+        className="site-page-header"
+        subTitle="Your personal Reef adobe"
+      ></PageHeader>
+
+      <PayStats />
+      <Card
+        title="Transactions
+    "
+        headStyle={{ color: "purple", fontWeight: 600 }}
+        bodyStyle={{}}
+      >
+        <Payments />
+        {/* <Dropdown overlay={menu} trigger={['click']}>
+          <button>
+          Merchant <DownOutlined />
+          </button>
+        </Dropdown> */}
+      </Card>
+    </>
   );
 };
 
